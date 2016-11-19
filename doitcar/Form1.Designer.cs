2b2dffc3e7266238ae501a660fe7833f580f0200
Zoltán László <pacee08@hotmail.com>
2016-11-19 14:57:09 +0100

    first commit

---
+++ doitcar/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace doitcar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblConnState = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lsbCommands = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtMultipler = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBackward = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 35);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(146, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblConnState
+            // 
+            this.lblConnState.AutoSize = true;
+            this.lblConnState.Location = new System.Drawing.Point(13, 19);
+            this.lblConnState.Name = "lblConnState";
+            this.lblConnState.Size = new System.Drawing.Size(81, 13);
+            this.lblConnState.TabIndex = 1;
+            this.lblConnState.Text = "Not connected.";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 64);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(147, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lsbCommands
+            // 
+            this.lsbCommands.FormattingEnabled = true;
+            this.lsbCommands.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.lsbCommands.Location = new System.Drawing.Point(12, 119);
+            this.lsbCommands.Name = "lsbCommands";
+            this.lsbCommands.Size = new System.Drawing.Size(41, 147);
+            this.lsbCommands.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 119);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 147);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0 - Stop\r\n1 - Forward\r\n2 - Backward\r\n3 - Left\r\n4 - Right\r\n6 - A Speed up\r\n7 - A S" +
+    "peed down\r\n8 - B Speed up\r\n9 - B Speed down";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(13, 291);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(146, 20);
+            this.txtIP.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(13, 339);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(146, 20);
+            this.txtPort.TabIndex = 7;
+            // 
+            // txtMultipler
+            // 
+            this.txtMultipler.Location = new System.Drawing.Point(33, 93);
+            this.txtMultipler.Name = "txtMultipler";
+            this.txtMultipler.Size = new System.Drawing.Size(126, 20);
+            this.txtMultipler.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "X";
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(243, 110);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(43, 36);
+            this.btnForward.TabIndex = 11;
+            this.btnForward.Text = "^";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnBackward
+            // 
+            this.btnBackward.Location = new System.Drawing.Point(243, 152);
+            this.btnBackward.Name = "btnBackward";
+            this.btnBackward.Size = new System.Drawing.Size(43, 36);
+            this.btnBackward.TabIndex = 12;
+            this.btnBackward.Text = "ˇ";
+            this.btnBackward.UseVisualStyleBackColor = true;
+            this.btnBackward.Click += new System.EventHandler(this.btnBackward_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(194, 152);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(43, 36);
+            this.btnLeft.TabIndex = 13;
+            this.btnLeft.Text = "<";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(292, 152);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(43, 36);
+            this.btnRight.TabIndex = 14;
+            this.btnRight.Text = ">";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(194, 194);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(141, 36);
+            this.btnStop.TabIndex = 15;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 409);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnBackward);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMultipler);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lsbCommands);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblConnState);
+            this.Controls.Add(this.btnConnect);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblConnState;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lsbCommands;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtMultipler;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBackward;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ doitcar/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace doitcar{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent();
+            txtIP.Text = "192.168.4.1";
+            txtPort.Text = "9003";
+            txtMultipler.Text = "10";
+            lsbCommands.SelectedIndex = 0;
+        }
+        
+        private int port;
+        private IPAddress ip;
+        private TcpClient client;
+        private NetworkStream stream;
+
+        //Csatlakozás a kocsihoz/serverhez
+        private void btnConnect_Click(object sender, EventArgs e){
+            try {
+                this.port = Convert.ToInt32(txtPort.Text);
+                this.ip = IPAddress.Parse(txtIP.Text);
+                this.client = new TcpClient();
+                client.Connect(ip, port);
+                lblConnState.Text = "Connected";
+            }catch(Exception exc){
+                MessageBox.Show("Connection failed\n" + exc.Message.ToString());
+            }
+        }
+
+        //Kézi parancsküldés
+        private void btnSend_Click(object sender, EventArgs e){
+            int commNumber = Convert.ToInt32(lsbCommands.SelectedItem);
+            int n = Convert.ToInt32(txtMultipler.Text);
+            sendCommand(commNumber, n);
+        }
+
+        //Irányító gomb események
+        private void btnStop_Click(object sender, EventArgs e)      {   sendCommand(0, 1);                                  }
+        private void btnForward_Click(object sender, EventArgs e)   {   sendCommand(1, Convert.ToInt32(txtMultipler.Text)); }
+        private void btnBackward_Click(object sender, EventArgs e)  {   sendCommand(2, Convert.ToInt32(txtMultipler.Text)); }
+        private void btnLeft_Click(object sender, EventArgs e)      {   sendCommand(3, Convert.ToInt32(txtMultipler.Text)); }
+        private void btnRight_Click(object sender, EventArgs e)     {   sendCommand(4, Convert.ToInt32(txtMultipler.Text)); }    
+
+        //Parancs kódok küldése n-szer
+        private void sendCommand(int commNumber, int n){
+            try{
+                Byte[] command = System.Text.Encoding.ASCII.GetBytes(commNumber.ToString());
+                this.stream = client.GetStream();
+                for (int i = 0; i < n; i++){
+                    this.stream.Write(command, 0, command.Length);
+                }
+            }catch(Exception exc){
+                MessageBox.Show("Sending failed\n" + exc.Message);
+                if (stream != null) stream.Close();
+                if (client != null) client.Close();
+            }
+        }     
+    }
+}
